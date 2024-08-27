ac64e4e9da733cfea23a5babf6cd139afac32c27
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-08-27 15:11:27 +0300

    Create Login Page UI

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react';
+import '../styles/Login.scss';
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div>
-      Login
+    <div className="login">
+      <div className="login_content">
+        <form className='login_content_form'>
+          <h1 style={{color: "white"}}>Login</h1>
+          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <button type="submit">Login</button>
+        </form>
+        <a href="">Dont have an account? Sign Up</a>
+      </div>
+      
     </div>
   )
 }
