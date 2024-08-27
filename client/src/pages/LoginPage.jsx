59f0f7d38061b36602df6d10633a653fb4b3ae32
Karem Saeed <95093124+karemsaeed20020@users.noreply.github.com>
2024-08-27 20:09:28 +0300

    Create Login Page Functionality and connect API

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,52 @@
 import { useState } from 'react';
 import '../styles/Login.scss';
+import { setLogin } from "../redux/state";
+import { useDispatch } from "react-redux"
+import { useNavigate } from "react-router-dom"
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch()
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const response = await fetch ("http://localhost:3001/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ email, password })
+      })
+
+      /* Get data after fetching */
+      const loggedIn = await response.json()
+
+      if (loggedIn) {
+        dispatch (
+          setLogin({
+            user: loggedIn.user,
+            token: loggedIn.token
+          })
+        )
+        navigate("/")
+      }
+
+    } catch (err) {
+      console.log("Login failed", err.message)
+    }
+  }
+
   return (
     <div className="login">
       <div className="login_content">
-        <form className='login_content_form'>
+        <form onSubmit={handleSubmit} className='login_content_form'>
           <h1 style={{color: "white"}}>Login</h1>
-          <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-          <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input type="email" name='email' placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input type="password" name='password' placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
           <button type="submit">Login</button>
         </form>
         <a href="">Dont have an account? Sign Up</a>
